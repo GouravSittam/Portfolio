28c4ec96eeb5928b0c45d6250185d8014eb4f020
Gourav Chaudhary <gouravsittam@gmail.com>
2025-05-31 13:25:51 +0530

    chore: update package dependencies and enhance layout with new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6 relative",
+          "min-h-screen bg-background font-sans antialiased max-w-3xl mx-auto py-12 sm:py-24 px-6 relative",
           fontSans.variable
         )}
       >
